c2d8a0dd19a35c99d685bf89a3852b6f43efa4c9
tanushree05 <singhtanushree111@gmail.com>
2024-12-28 17:13:24 +0530

    pattern program 2

--- Basic_practice_code/Example_13.java
+++ Basic_practice_code/Example_13.java
@@ -7,8 +7,10 @@ package Basic_practice_code;
   */
 public class Example_13 {
     public static void main(String[] args) {
-        for(int i=0;i<=3;i++){
-            for(int j=0;j<=4;j++){
+        int n=4;
+        int m=5;
+        for(int i=0;i<=n-1;i++){
+            for(int j=0;j<=m-1;j++){
                 System.out.print("*"+" ");
             }
             System.out.println();
---
+++ Basic_practice_code/Example_14.java
@@ -0,0 +1,29 @@
+package Basic_practice_code;
+/*
+print the pattern 
+* * * * * * 
+*         * 
+*         *
+* * * * * *
+ */
+public class Example_14 {
+    public static void main(String[] args) {
+       int  n=4;
+       int m=6;
+        
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i ==1 ||j==1|| j== m|| i == n){
+                    System.out.print("*"+" ");
+                 }
+                 else{
+                    System.out.print(" "+" ");
+                 }
+
+            }
+            System.out.println();
+        }
+    }
+    
+}
