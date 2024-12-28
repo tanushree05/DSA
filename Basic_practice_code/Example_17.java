5608a7464067d6a2b814aadf9fd56484d2f83fd1
tanushree05 <singhtanushree111@gmail.com>
2024-12-28 18:28:25 +0530

    pattern program

---
+++ Basic_practice_code/Example_17.java
@@ -0,0 +1,24 @@
+package Basic_practice_code;
+/* 
+Write a program to print following pattern.
+1 
+1 2
+1 2 3
+1 2 3 4
+
+ */
+public class Example_17 {
+    public static void main(String[] args) {
+       int n=4;
+
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print(j+" ");
+        }
+        System.out.println();
+       }
+
+
+    }
+    
+}
