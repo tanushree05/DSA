290a2eb7047429df64018c3884c16dd4cb750c7d
tanushree05 <singhtanushree111@gmail.com>
2024-12-28 16:47:26 +0530

    pattern program * * * * * * * * * * * * * * * * * * * *

---
+++ Basic_practice_code/Example_13.java
@@ -0,0 +1,17 @@
+package Basic_practice_code;
+ /* Write a program to print 
+* * * * * 
+* * * * *
+* * * * *
+* * * * *
+  */
+public class Example_13 {
+    public static void main(String[] args) {
+        for(int i=0;i<=3;i++){
+            for(int j=0;j<=4;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
